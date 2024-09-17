26e160b8d01bf625c53d50b901af20d1c09dfaec
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2024-09-18 00:04:57 +0300

    AnalyticsModule

--- apps/admin-api/src/admin-api.module.ts
+++ apps/admin-api/src/admin-api.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AdminApiController } from './admin-api.controller';
 import { AdminApiService } from './admin-api.service';
+import { VendorsModule } from './vendors/vendors.module';
+import { OrdersModule } from './orders/orders.module';
+import { CatalogModule } from './catalog/catalog.module';
+import { SettingsModule } from './settings/settings.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { LoggerModule } from './logger/logger.module';
+import { AnalyticsModule } from './analytics/analytics.module';
+import { SupportModule } from './support/support.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
-  imports: [],
+  imports: [VendorsModule, OrdersModule, CatalogModule, SettingsModule, ReviewsModule, LoggerModule, AnalyticsModule, SupportModule, DashboardModule],
   controllers: [AdminApiController],
   providers: [AdminApiService],
 })
