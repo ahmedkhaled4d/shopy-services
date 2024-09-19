04ff2d61f12a523861ad2b71e4495d056c4bfc2a
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2024-09-19 19:31:36 +0300

    vendor modules

--- apps/vendor-api/src/vendor-api.module.ts
+++ apps/vendor-api/src/vendor-api.module.ts
@@ -5,6 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from '@app/database';
 import { AuthModule } from './auth/auth.module';
+import { OrderModule } from './order/order.module';
+import { CatalogModule } from './catalog/catalog.module';
+import { DesignModule } from './design/design.module';
+import { ReviewModule } from './review/review.module';
+import { MarketingModule } from './marketing/marketing.module';
+import { AnalyticsModule } from './analytics/analytics.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { AccountModule } from './account/account.module';
 
 @Module({
   imports: [
@@ -18,6 +26,14 @@ import { AuthModule } from './auth/auth.module';
     }),
     AuthModule,
     DatabaseModule,
+    OrderModule,
+    CatalogModule,
+    DesignModule,
+    ReviewModule,
+    MarketingModule,
+    AnalyticsModule,
+    DashboardModule,
+    AccountModule,
   ],
   controllers: [VendorApiController],
   providers: [VendorApiService],
