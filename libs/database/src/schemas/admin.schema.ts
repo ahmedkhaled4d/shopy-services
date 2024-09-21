3b216eac8202206e4da460060f379ca64ae82a59
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2024-09-22 00:49:46 +0300

    AdminDocument

---
+++ libs/database/src/schemas/admin.schema.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+export type AdminDocument = Admin & Document;
+
+@Schema()
+export class Admin {
+  _id: Types.ObjectId;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  phone: string;
+
+  @Prop()
+  tempToken?: {
+    token?: string;
+    expire?: Date;
+  };
+
+  @Prop()
+  refreshToken?: string;
+}
+
+export const AdminSchema = SchemaFactory.createForClass(Admin);
--- libs/database/src/schemas/user.schema.ts
+++ libs/database/src/schemas/user.schema.ts
@@ -13,6 +13,53 @@ export class User {
   @Prop({ required: true })
   password: string;
 
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  phone: string;
+
+  @Prop({ required: true })
+  timezone: string;
+
+  @Prop({ default: 'basic' })
+  plan: string;
+
+  @Prop()
+  tempToken?: {
+    token?: string;
+    expire?: Date;
+  };
+
+  @Prop({ default: { plan: 'basic' } })
+  store?: {
+    name?: string;
+    logo?: string;
+    link?: string;
+    region?: string;
+    category?: string;
+    currency?: string;
+    taxs: {
+      rate: number;
+      method: string;
+      id: string;
+    };
+    communities: {
+      whatsapp_link: string;
+      telegram_link: string;
+      instegram_link: string;
+      facebook_link: string;
+    };
+  };
+
+  @Prop({ default: { isShowLogo: false, isShowEmail: false } })
+  invoice?: {
+    isShowLogo: boolean;
+    isShowEmail: boolean;
+    header: string;
+    footer: string;
+  };
+
   @Prop()
   refreshToken?: string;
 }
